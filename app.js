04a42942433cc7ff7a82ed93bc270d36a322ff95
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-07 17:22:43 +0530

    main app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer"); 
+
+// const uplodad_data = multer()
+const user_router = require("./routes/user_login_routes");
+const doctor_router = require("./routes/doctor_login_routes");
+const appointment_router = require("./routes/appointment_booking_routes");
+const order_router = require("./routes/order_medicine_routes");
+
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+// app.use(uplodad_data.array())
+
+app.use('/api' ,user_router);
+app.use('/api',doctor_router);
+app.use(appointment_router);
+app.use(order_router);
+
+const PORT = 6000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose.connect(process.env.Mongo_URL)
+    .then(() => {
+        console.log("Connected to database successfully");
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database", err);
+    });
