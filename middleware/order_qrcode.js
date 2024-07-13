5b80b7607e7c2b5f1bd099c4e8e4737a0bf72949
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-13 19:44:41 +0530

    order qr code middleware update

--- middleware/order_qrcode.js
+++ middleware/order_qrcode.js
@@ -1,17 +1,20 @@
-const qrcode = require("qrcode")
+const qrcode = require("qrcode");
+const path = require("path");
 
-async function generateQrCode(pname , phone_no , emailId , medicines){
-    try {
-        const qrCodeData = `Patient Name : ${pname}\n Phone No : ${phone_no}\n Email Id : ${emailId} \n Medicines : ${medicines}`
-        const qrCodePath = `../order_qrcodes_data/${pname}`
+async function generateQrCode(pname , phone_no , emailId , medicines) {
+  try {
+    const medicinesStr = medicines.map(med => `${med.medicine_name} (Qty: ${med.qty})`).join(", ");
+    const qrCodeData = `Patient Name: ${pname}\n Phone Number: ${phone_no} \n Email ID : ${emailId} \n Medicines : ${medicinesStr}`;
+    const qrCodeFileName = `${pname}-${phone_no}.png`;
+    const qrCodePath = path.join(__dirname, `../order_qrcodes_data/${qrCodeFileName}`);
 
-        await qrcode.toFile(qrCodePath , qrCodeData)
-        
-        return qrCodePath
-        
-    } catch (error) {
-        console.log(error)
-    }
+    await qrcode.toFile(qrCodePath, qrCodeData);
+
+    return qrCodePath;
+  } catch (error) {
+    console.error("Error generating QR code:", error);
+    throw new Error("Failed to generate QR code");
+  }
 }
 
-module.exports = {generateQrCode}
+module.exports = { generateQrCode };
