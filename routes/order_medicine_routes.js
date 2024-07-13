5f11cdbec1cdb16229e58ffb0fd27d253bf02a7e
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-13 19:42:52 +0530

    order medicine routes update

--- routes/order_medicine_routes.js
+++ routes/order_medicine_routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const order_control = require("../controllers/order-medicine-controller");
+const store_img = require("../middleware/prescription_image")
 
 const order_router = express.Router();
 
 order_router.get("/doctor_login/:loginId/orders", order_control.getAllOrders);
 order_router.get("/doctor_login/:loginId/orders/:orderId", order_control.getOrderById);
-order_router.post("/user_login/:loginId/place_order", order_control.placeOrder);
+order_router.post("/patient_login/:loginId/place_order", store_img ,  order_control.placeOrder);
 
 module.exports = order_router;
