13c63a7dd0df4d3c1c388281b6d823987b64c060
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-13 19:44:03 +0530

    prescription image middleware update

--- middleware/prescription_image.js
+++ middleware/prescription_image.js
@@ -3,38 +3,31 @@ const path = require("path");
 
 const prescription_img_dir = path.join(__dirname, "../prescription_images/");
 
-async function store_image(pname) {
-  const Storage = multer.diskStorage({
-    destination: (req, prescription_img, cb) => {
-      cb(null, prescription_img_dir);
-    },
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, prescription_img_dir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const fileName = `${req.body.pname}-${req.body.phone_no}${ext}`;
+    cb(null, fileName);
+  },
+});
 
-    filename: async (req, prescription_img, cb) => {
-      const ext = path.extname(prescription_img.originalname);
-      const FileName = `${pname}-${ext}`;
-      cb(null, FileName);
-    },
-  });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/jpg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      cb(new Error("Only JPG, JPEG, and PNG file types are allowed"));
+    }
+  },
+});
 
-  const upload = multer({
-    storage: Storage,
-    fileFilter: (req, user_img, cb) => {
-      if (
-        user_img.mimetype == "image/png" ||
-        user_img.mimetype == "image/jpeg" ||
-        user_img.mimetype == "image/jpg"
-      ) {
-        cb(null, true);
-      } else {
-        cb(null, false);
-        console.log("Wrong file type alert");
-
-        return cb(alert("Only JPG , JPEG and PNG file types are allowed"));
-      }
-    },
-  });
-
-  return upload;
-}
-
-module.exports = { store_image };
+module.exports = upload.single("prescription");
